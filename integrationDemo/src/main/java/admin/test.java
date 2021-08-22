197c6d39c080b67f570255e4e75815f15f9f8faf
emad abdelkawy <emado600@gmail.com>
2021-08-22 16:48:43 +0300

    initial Demo App

---
+++ integrationDemo/src/main/java/admin/test.java
@@ -0,0 +1,12 @@
+package admin;
+
+public class test {
+	
+	
+	public static void main(String args[]) 
+	{ 
+		System.out.println("My first program in Java, HelloWorld !!");
+	}
+	
+
+}
---
+++ javaApp/src/javaApp1/test.java
@@ -0,0 +1,12 @@
+package javaApp1;
+
+public class test {
+	
+	
+	public static void main(String args[]) 
+	{ 
+		System.out.println("integrate Jenkins with git !!");
+	}
+	
+
+}
